91777d92a149ac479f10550e5540aabeca1bede8
kinfey <kinfeylo@microsoft.com>
2024-05-06 11:03:53 +0800

    add ios sample codes

---
+++ ios/GenAIApp/GenAIApp/ViewController.mm
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  GenAIApp
+//
+//  Created by lokinfey on 5/6/24.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
